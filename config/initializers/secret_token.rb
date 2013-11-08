b120d4f1bfae81635296d2e2ea922168b18935b3
parker <parkerzhou2010@gmail.com>
2013-11-08 15:32:06 +0800

    ready to push to heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zhanyiutil::Application.config.secret_key_base = 'b90951860576b539a1adfcfe81316b3315e26fc019524d1cf037f5096f866448bdc9d6928186f0873c69abd2d7bbc9cd0cb227a5c92a5b1bd2449c76bd08c3cb'
